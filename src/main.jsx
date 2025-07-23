198a517cc30dc5735ccb5eb6416585caf5ef3da6
roqaya-k7an <153377899+roqaya-k7an@users.noreply.github.com>
2025-07-23 07:13:35 +0000

    Updated FAQs styles and main logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// Optional: add performance reporting or monitoring (e.g., Google Analytics, Sentry)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
